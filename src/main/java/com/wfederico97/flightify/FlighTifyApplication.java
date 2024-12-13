fe1b1e4cd544b92396f0708ac8dfaa60771eee64
Federico Wuthrich <wuthrich09@gmail.com>
2024-12-12 23:06:37 -0300

    Project files

---
+++ src/main/java/com/wfederico97/flightify/FlighTifyApplication.java
@@ -0,0 +1,13 @@
+package com.wfederico97.flightify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlighTifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlighTifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wfederico97/flightify/FlighTifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfederico97.flightify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlighTifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
